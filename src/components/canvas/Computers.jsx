cc410a13714daf9d07a869baca37f8d684f32963
Mareline <mrami204@fiu.edu>
2023-09-04 00:40:11 -0400

    Adjusted hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,47 +1,56 @@
-import React, { Suspense, useEffect, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-//helpers to allow to draw on the canvas
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from '../Loader';
+import CanvasLoader from "../Loader";
 
-const Computers = ({isMobile}) => {
-  const computer =  useGLTF("./desktop_pc/scene.gltf");
+const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      {/* creating the glare on the computer*/}
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <pointLight intensity={1} />
       <primitive
-      object={computer.scene}/>
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
-  return(
-    <Canvas
+  return (
+    <div>
+      <Suspense fallback={<CanvasLoader />}>
+      <Canvas
       frameloop='demand'
       shadows
-      // Where are looking at the model from ___:[x,y,z]
-      camera={{ position:[20, 3, 5], fov: 25 }}
-      // To perfectly render the model
-      gl={{ preserveDrawingBuffer: true}}
-      >
-        {/* Adding the suspense from React to have a loader while the  model is loading */}
-        <Suspense fallback={<CanvasLoader />}>
-            {/* This will allow us to move the model from left to right and from a specific access */}
-            <OrbitControls 
-            enableZoom={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-            />
-          <Computers />
-        </Suspense>
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      <Preload all />
+    </Canvas>
+    </Suspense>
+    </div>
+  );
+};
 
-        <Preload all />
-      </Canvas>
-  )
-}
-export default Computers
+export default ComputersCanvas;
