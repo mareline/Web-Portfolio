9db9e2f98f101226660dbaf23f812e24c28dddcb
Mareline <mrami204@fiu.edu>
2023-09-10 04:59:14 -0400

    Updated hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,32 @@ import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
     <nav
-      className={'${styles.paddingX}} w-full flex items-center py-5 fixed top-0 z-20 bg-primary'}
+    className={`${
+      styles.paddingX
+    } w-full flex items-center py-5 fixed top-0 z-20 ${
+      scrolled ? "bg-primary" : "bg-transparent"
+    }`}
     >
       <div className="w-full flex justify-between items-center max-w-7l mx-auto">
         <Link
@@ -20,10 +43,12 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
           >
-            <img src={logo} alt="logo" className="w-10 h-10" />
-            <p className="text-white text-[18px] font-bold cursor-pointer"> Mareline Ramirez </p>
+            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <p className="text-white text-[18px] font-bold cursor-pointer flex"> Mareline Ramirez &nbsp; </p>
         </Link>
-        <ul className="list-none sm:flex flex-row gap-10">
+
+        {/* Desktop application navbar */}
+        <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((nav) => (
             <li
               key={nav.id}
@@ -31,12 +56,47 @@ const Navbar = () => {
               className={`${
                 active === nav.title ? "text-white" : "text-secondary"
               } hover:text-white text-[18px] font-medium cursor-pointer`}
+              /* Shows where you are on the link */
               onClick={() => setActive(nav.title)}
             >
               <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
         </ul>
+        
+        {/* Mobile application navbar */}
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
+          />
+          {/* Mobile application menu */}
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            {/* Rendering the elements About, etc. */}
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
 
     </nav>
