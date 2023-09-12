ce582369043d298983c8a95389e1dfec3c6443a4
Mareline <mrami204@fiu.edu>
2023-09-12 04:28:17 -0400

    Updated Bio

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,8 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import ProfilePic from "./ProfilePic.png";
+
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -71,4 +73,5 @@ export {
   RockPaper,
   Portfolio,
   Bookstore,
+  ProfilePic,
 };
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -50,13 +50,13 @@ const About = () => {
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
 
-        I am a computer science student with a deep commitment to fostering diversity and inclusion within the tech industry. My expertise merges strong leadership and technical skills.
+        I am a computer science student with a deep commitment to fostering diversity and inclusion within the tech industry. My expertise merges strong leadership and technical skills. <br />
 
-        Currently, I serve as the Technical Lead and Industry Relations coordinator at WiCS FIU (Women in Computer Science). My role involves actively shaping gender diversity and inclusion through impactful workshops for students at FIU, achieved through cross-functional collaboration. I served as the previous Director at TechTogether Miami, where I have successfully organized a gender-focused hackathon, representing marginalized gender students through effective project management, budgeting, and community engagement. 
+        <br />Currently, I serve as the Technical Lead and Industry Relations coordinator at WiCS FIU (Women in Computer Science). My role involves actively shaping gender diversity and inclusion through impactful workshops for students at FIU, achieved through cross-functional collaboration. I served as the previous Director at TechTogether Miami, where I have successfully organized a gender-focused hackathon, representing marginalized gender students through effective project management, budgeting, and community engagement.<br />
 
-        I further honed my technical skills during Capital One's Tech Mini-Mester, gaining practical experience in Node, API, Arrays, Objects, and Iterations, while expanding my industry network. My participation in Microsoft's Tech Resilience Mentoring Program and personal projects such as the 100-Days-of-Code initiative continually elevate my programming skills.
+       <br /> I further honed my technical skills during Capital One's Tech Mini-Mester, gaining practical experience in Node, API, Arrays, Objects, and Iterations, while expanding my industry network. My participation in Microsoft's Tech Resilience Mentoring Program and personal projects such as the 100-Days-of-Code initiative continually elevate my programming skills. <br />
 
-        I am fluent in English and Spanish, and am skilled in Java, Javascript, HTML, CSS, MySQL, and Python.
+       <br /> I am fluent in English and Spanish, and am skilled in Java, Javascript, HTML, CSS, MySQL, and Python.
       </motion.p>
       
       <div className='mt-20 flex flex-wrap gap-10 place-content-center'>
